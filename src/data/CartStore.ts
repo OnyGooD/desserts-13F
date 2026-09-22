1f1afa19ca96d72984d8ac6acb4675edfcf63175
OnyGooD <molnekkrisz@gmail.com>
2026-09-22 09:15:43 +0200

    Refactor cart functionality to use Zustand for state management and update order confirmation flow

--- src/data/CartStore.ts
+++ src/data/CartStore.ts
@@ -10,13 +10,21 @@ type CartState = {
     clearCart: () => void
 }
 
-export const useCartStore = create<CartState>((set)=>({
+export const useCartStore = create<CartState>((set) => ({
     items: [],
-    addItem: (dessert) => set((state)=>({
-        items: [...state.items, {...dessert, quantity: 1}]
+    addItem: (dessert) => set((state) => ({
+        items: [...state.items, { ...dessert, quantity: 1 }]
     })),
-    incrementItem: (name) => set((state)=>({})),
-    decrementItem: (name) => set((state)=>({})),
-    removeItem: (name) => set((state)=>({})),
-    clearCart: () => set((state)=>({})),
+    incrementItem: (name) => set((state) => ({
+        items: state.items.map((item) => item.name === name ? { ...item, quantity: item.quantity + 1 } : item)
+    })),
+    decrementItem: (name) => set((state) => ({
+        items: state.items.map((item) => item.name === name ? { ...item, quantity: Math.max(0, item.quantity - 1) } : item)
+    })),
+    removeItem: (name) => set((state) => ({
+        items: state.items.filter((item) => item.name !== name)
+    })),
+    clearCart: () => set((state) => ({
+        items: []
+    })),
 }))
